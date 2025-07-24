32b52730cab39cd72f52c0d112ac69511a3e09b2
astridrodriguezhdz <rdoriguezines@gmail.com>
2025-07-24 14:24:25 -0600

    Fixing errors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,9 @@ const today = new Date();
 //  To get the current year
 //current_year.innerHTML = `getFullYear(): ${today.getFullYear()}`;  // the ` ` is the space for text. getFullYear(): could be any text, and ${today.getFullYear()} is th code to show the current year.
 current_year.innerHTML = `${today.getFullYear()}`;  // Just show 2025
+
+// To get the last modified date
+let last_modified = new Date(document.lastModified);
+document.querySelector(
+  "#last_modified"
+).textContent = `Última modificación: ${last_modified.toLocaleString("es-MX")}`;
